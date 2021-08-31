b7b903c87cdc0cf768b12cd319729b1f1e5cf214
Stu Yates <syates@agc.com>
2021-08-31 15:40:13 -0600

    Fixed issue: DB passwords in settings.py did not match docker-compose file

--- app/settings.py
+++ app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'reagent_portal_dev',
+        'USER': 'reagent_portal',
+        'PASSWORD': 'reagent_portal',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
